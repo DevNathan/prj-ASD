eaead02697e6cbaa1aa6aa5021ca26b8c71c82ef
nathan <devnamho0910@gmail.com>
2024-11-19 20:21:26 +0900

    Edit: WS 리스폰스 처리 수정

--- client/src/hooks/useWebSocket.ts
+++ client/src/hooks/useWebSocket.ts
@@ -9,13 +9,31 @@ const useWebSocket = (url: string): UseWebSocketResult => {
   const [messages, setMessages] = useState<ChatMessage[]>([]);
   const socketRef = useRef<WebSocket | null>(null);
 
+  // 받은 메시지는 STT: {text} 혹은 TTS: {text}로 올 것.
+  function transformMessage(rawMessage: string): ChatMessage | null {
+    if (rawMessage.startsWith("STT:")) {
+      return {
+        type: "USER",
+        message: rawMessage.replace("STT:", "").trim(),
+      };
+    } else if (rawMessage.startsWith("TTS:")) {
+      return {
+        type: "BOT",
+        message: rawMessage.replace("TTS:", "").trim(),
+      };
+    }
+    return null;
+  }
+
   // 웹소켓 연결 및 메시지 수신 처리
   useEffect(() => {
     socketRef.current = new WebSocket(url);
 
     socketRef.current.onmessage = (e) => {
-      const data: ChatMessage = JSON.parse(e.data);
-      setMessages((prev) => [...prev, data]);
+      const transformedMessage = transformMessage(e.data);
+      if (transformedMessage) {
+        setMessages((prev) => [...prev, transformedMessage]);
+      }
     };
 
     return () => {
